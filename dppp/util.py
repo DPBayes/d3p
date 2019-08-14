fix: assert in logic_regression.model was failing in per example gradient computation

dppp/util.py
@@ -53,7 +53,7 @@ def example_count(a):
     try:
         return np.shape(a)[0]
     except:
-        raise ValueError("given batch was not an array with at least one dimension")
+        return 1
 
 
 def has_shape(a):

examples/logistic_regression.py
@@ -23,6 +23,7 @@ import jax
 import numpyro.distributions as dist
 from numpyro.handlers import param, sample, seed, substitute
 
+from dppp.util import example_count
 from dppp.svi import per_example_elbo, dpsvi, minibatch
 from numpyro.svi import elbo
 
@@ -37,8 +38,18 @@ def model(batch_X, batch_y=None, num_obs_total=None):
     :param batch_X: a batch of predictors
     :param batch_y: a batch of observations
     """
-    assert(batch_y is None or batch_X.shape[0] == batch_y.shape[0])
+    # note(lumip): the following if construct is currently necessary because
+    #   the per-example value_and_grad function uses vmap internally, applying
+    #   model and guide to 1-example batches (and stripping the first dimension)
+    #   this is not nice because it means that model/guide have to be adapted
+    #   if they do the kind of checks as below..
+    if batch_X.ndim == 2:
+        assert(batch_y is None or example_count(batch_X) == example_count(batch_y))
+    elif batch_X.ndim == 1:
+        assert(batch_y is None or example_count(batch_y) == 1)
+
     z_dim = np.atleast_2d(batch_X).shape[1]
+
     z_w = sample('w', dist.Normal(np.zeros((z_dim,)), np.ones((z_dim,)))) # prior is N(0,I)
     z_intercept = sample('intercept', dist.Normal(0,1)) # prior is N(0,1)
     logits = batch_X.dot(z_w)+z_intercept