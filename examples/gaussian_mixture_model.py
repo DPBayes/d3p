fix: adapted all examples for correct sample scaling

examples/gaussian_mixture_model.py
@@ -48,30 +48,31 @@ def model(k, obs_or_shape, num_obs_total=None):
 
     if isinstance(obs_or_shape, tuple):
         assert(len(obs_or_shape) == 2)
-        N, d = obs_or_shape
+        batch_size, d = obs_or_shape
         obs = None
     else:
         obs = obs_or_shape
         assert(obs is not None)
-        assert(len(obs.shape) <= 2)
-        N, d = np.atleast_2d(obs).shape
+        assert(np.ndim(obs) <= 2)
+        # np.atleast_2d necessary because batch_size dimension is strapped during gradient computation
+        batch_size, d = np.shape(np.atleast_2d(obs))
 
     alpha = np.ones(k)*0.3
     a0, b0 = np.ones((k,1))*.5, np.ones((k,1))*.5
 
-    pis = np.broadcast_to(sample('pis', dist.Dirichlet(alpha)), (N,k))
+    pis = np.broadcast_to(sample('pis', dist.Dirichlet(alpha)), (batch_size,k))
     mus = sample('mus', dist.Normal(np.zeros((k, d)), 10.))
     sigs = sample('sigmas', dist.Gamma(a0, b0))
 
-    assert(pis.shape == (N, k))
+    assert(pis.shape == (batch_size, k))
     assert(np.atleast_2d(mus).shape == (k, d))
     assert(np.atleast_1d(sigs).shape[0] == k)
 
-    with minibatch(obs, num_obs_total=num_obs_total):
+    with minibatch(batch_size, num_obs_total=num_obs_total):
         z = sample('z', dist.Categorical(pis)).flatten()
-        assert(z.shape == (N,))
-        X = sample('obs', dist.Normal(mus[z], sigs[z]), obs=obs, sample_shape=N)
-        assert(np.atleast_2d(X).shape == (N, d))
+        assert(z.shape == (batch_size,))
+        X = sample('obs', dist.Normal(mus[z], sigs[z]), obs=obs, sample_shape=batch_size)
+        assert(np.atleast_2d(X).shape == (batch_size, d))
 
     return X
 
@@ -79,14 +80,22 @@ def model(k, obs_or_shape, num_obs_total=None):
 def compute_assignment_log_posterior(k, obs, mus, sigs, pis_prior):
     # computes the unnormalized log-posterior for each value of assignment z
     #   for each data point
-    N = np.atleast_1d(obs).shape[0]
+    assert(np.ndim(obs) <= 2)
+    if np.ndim(obs) == 2:
+        batch_size = np.shape(obs)[0]
+    else:
+        shape = np.shape(obs)
+        obs = np.reshape(obs, (1, *shape))
+        batch_size = 1
 
     def per_component_fun(j):
-        log_prob_x_zj = np.sum(dist.Normal(mus[j], sigs[j]).log_prob(obs), axis=1).flatten()
-        assert(np.atleast_1d(log_prob_x_zj).shape == (N,))
+        log_prob_x_zj = np.sum(
+            dist.Normal(mus[j], sigs[j]).log_prob(obs), axis=1
+        ).flatten()
+        assert(np.atleast_1d(log_prob_x_zj).shape == (batch_size,))
         log_prob_zj = dist.Categorical(pis_prior).log_prob(j)
         log_prob = log_prob_x_zj + log_prob_zj
-        assert(np.atleast_1d(log_prob).shape == (N,))
+        assert(np.atleast_1d(log_prob).shape == (batch_size,))
         return log_prob
 
     z_log_post = jax.vmap(per_component_fun)(np.arange(k))
@@ -105,7 +114,6 @@ def estimate_pis(assignment_log_posterior):
     # Note that we exponentiate the log-ratios, which could still lead to
     #   numerical inaccuracies. However, we can safely clip the ratio here
     #   without losing information (which wouldn't work for the posteriors).
-
     N, k = np.atleast_2d(assignment_log_posterior).shape
     z_log_post = assignment_log_posterior.T
 
@@ -128,8 +136,12 @@ def guide(k, obs, num_obs_total=None):
     :param obs: observed samples to condition the model with
     """
     assert(obs is not None)
-    assert(len(obs.shape) <= 2)
-    N, d = np.atleast_2d(obs).shape
+    assert(np.ndim(obs) <= 2)
+    if np.ndim(obs) == 2:
+        batch_size, d = np.shape(obs)
+    else:
+        d = np.shape(obs)[0]
+        batch_size = 1
 
     a0, b0 = param('a0', np.ones((k, 1))*.5), param('b0', np.ones((k, 1))*.5)
     alpha = param('alpha', np.ones(k)*0.3)
@@ -148,9 +160,9 @@ def guide(k, obs, num_obs_total=None):
     # from z posterior probabilities to pis
     pis_post = estimate_pis(z_log_post)
     # we require a z for each example. ensure that pis is of correct shape
-    assert(np.atleast_2d(pis_post).shape == (N, k))
+    assert(np.atleast_2d(pis_post).shape == (batch_size, k))
 
-    with minibatch(obs, num_obs_total=num_obs_total):
+    with minibatch(batch_size, num_obs_total=num_obs_total):
         z = sample('z', dist.Categorical(pis_post)).flatten()
     return pis_post, z, mus, sigs
 
@@ -193,6 +205,7 @@ def main(args):
 
     X_train, X_test, latent_vals = create_toy_data(N, k_gen, d)
     train_init, train_fetch = batchify_data((X_train,), args.batch_size)
+    test_init, test_fetch = batchify_data((X_test,), args.batch_size)
 
     ## Init optimizer and training algorithms
     opt_init, opt_update, get_params = optimizers.adam(args.learning_rate)
@@ -226,28 +239,28 @@ def main(args):
     @jit
     def epoch_train(rng, opt_state, data_idx, num_batch):
         def body_fn(i, val):
-            opt_state, rng = val
+            loss, opt_state, rng = val
             rng, update_rng = random.split(rng, 2)
             batch = train_fetch(i, data_idx)
-            _, opt_state, rng = svi_update(
+            batch_loss, opt_state, rng = svi_update(
                 i, update_rng, opt_state, batch, batch
             )
-            return opt_state, rng
+            loss += batch_loss / (args.num_samples * num_batch)
+            return loss, opt_state, rng
 
-        return lax.fori_loop(0, num_batch, body_fn, (opt_state, rng))
+        return lax.fori_loop(0, num_batch, body_fn, (0., opt_state, rng))
 
 
     @jit
     def eval_test(rng, opt_state, data_idx, num_batch):
         def body_fn(i, val):
             loss_sum, rng = val
-            batch = train_fetch(i, data_idx)
-            loss = svi_eval(rng, opt_state, batch, batch) / args.num_samples
-            loss_sum += loss
+            batch = test_fetch(i, data_idx)
+            loss = svi_eval(rng, opt_state, batch, batch)
+            loss_sum += loss / (args.num_samples * num_batch)
             return loss_sum, rng
 
         loss, _ = lax.fori_loop(0, num_batch, body_fn, (0., rng))
-        loss = loss / num_batch
         return loss
 
     smoothed_loss_window = onp.empty(5)
@@ -259,22 +272,23 @@ def main(args):
         t_start = time.time()
         rng, data_fetch_rng, test_rng = random.split(rng, 3)
 
-        num_train, train_idx = train_init(rng=data_fetch_rng)
-        opt_state, rng = epoch_train(
-            rng, opt_state, train_idx, num_train
+        num_train_batches, train_idx = train_init(rng=data_fetch_rng)
+        train_loss, opt_state, rng = epoch_train(
+            rng, opt_state, train_idx, num_train_batches
         )
 
         if i % 100 == 0:
-            # computing loss over training data (for now?)
+            rng, test_rng, test_fetch_rng = random.split(rng, 3)
+            num_test_batches, test_idx = test_init(rng=test_fetch_rng)
             test_loss = eval_test(
-                test_rng, opt_state, train_idx, num_train
+                test_rng, opt_state, test_idx, num_test_batches
             )
             smoothed_loss_window[window_idx] = test_loss
             smoothed_loss = onp.nanmean(smoothed_loss_window)
             window_idx = (window_idx + 1) % 5
 
-            print("Epoch {}: loss = {}, smoothed loss = {} ({:.2f} s.)".format(
-                    i, test_loss, smoothed_loss, time.time() - t_start
+            print("Epoch {}: loss = {} (smoothed = {}) (on training set = {}) ({:.2f} s.)".format(
+                    i, test_loss, smoothed_loss, train_loss, time.time() - t_start
                 ))
 
     params = get_params(opt_state)
@@ -285,8 +299,8 @@ def main(args):
 
     # getting accuracy score for attributing data to the mixture components
     # based on the learned model
-    original_assignment = latent_vals[0]
-    original_modes = latent_vals[1]
+    original_assignment = latent_vals[1]
+    original_modes = latent_vals[2]
     # we first map our true modes to the ones learned in the model using the
     # log posterior for z
     mode_assignment_posterior = compute_assignment_log_posterior(k, original_modes, params['mus_loc'], np.ones((k, d)), dist.Dirichlet(params['alpha']).mean)
@@ -299,13 +313,13 @@ def main(args):
     
     # we next obtain the assignments for the data according to the model and
     # pass them through the inverse map we just build
-    post_data_assignment = compute_assignment_log_posterior(k, X, params['mus_loc'], np.ones((k, d)), dist.Dirichlet(params['alpha']).mean)
+    post_data_assignment = compute_assignment_log_posterior(k, X_test, params['mus_loc'], np.ones((k, d)), dist.Dirichlet(params['alpha']).mean)
     post_data_assignment = np.argmax(post_data_assignment, axis=1)
     remapped_data_assignment = np.array([inv_mode_map[j] for j in post_data_assignment._value])
 
     # finally, we can compare the results with the original assigments and compute
     # the accuracy
-    acc = np.sum(original_assignment == remapped_data_assignment)/X.shape[0]
+    acc = np.sum(original_assignment == remapped_data_assignment)/X_test.shape[0]
     print("assignment accuracy: {}".format(acc))
     
 if __name__ == "__main__":

examples/gaussian_mixture_model2.py
@@ -80,17 +80,21 @@ def model(k, obs, num_obs_total=None):
     # this is our model function using the MixGaus distribution defined above
     # with prior belief
     assert(obs is not None)
-    _, d = np.atleast_2d(obs).shape
+    assert(np.ndim(obs) <= 2)
+    # np.atleast_2d necessary because batch_size dimension is strapped during gradient computation
+    batch_size, d = np.shape(np.atleast_2d(obs))
+
     pis = sample('pis', dist.Dirichlet(np.ones(k)))
     mus = sample('mus', dist.Normal(np.zeros((k, d)), 10.))
     sigs = np.ones((k, d))
-    with minibatch(obs, num_obs_total=num_obs_total):
+    with minibatch(batch_size, num_obs_total=num_obs_total):
         return sample('obs', MixGaus(mus, sigs, pis), obs=obs)
 
 def guide(k, obs, num_obs_total=None):
     # the latent MixGaus distribution which learns the parameters
     assert(obs is not None)
-    _, d = np.atleast_2d(obs).shape
+    _, d = np.shape(np.atleast_2d(obs))
+
     mus_loc = param('mus_loc', np.zeros((k, d)))
     mus = sample('mus', dist.Normal(mus_loc, 1.))
     sigs = np.ones((k, d))
@@ -191,7 +195,7 @@ def main(args):
 
     X_train, X_test, latent_vals = create_toy_data(N, k_gen, d)
     train_init, train_fetch = batchify_data((X_train,), args.batch_size)
-    test_init, test_fetch = batchify_data((X_train,), args.batch_size)
+    test_init, test_fetch = batchify_data((X_test,), args.batch_size)
 
     ## Init optimizer and training algorithms
     opt_init, opt_update, get_params = optimizers.adam(args.learning_rate)
@@ -231,7 +235,7 @@ def main(args):
             batch_loss, opt_state, rng = svi_update(
                 i, update_rng, opt_state, batch, batch
             )
-            loss += batch_loss / (args.num_samples * args.batch_size * num_batch)
+            loss += batch_loss / (args.num_samples * num_batch)
             return loss, opt_state, rng
 
         return lax.fori_loop(0, num_batch, body_fn, (0., opt_state, rng))
@@ -241,8 +245,8 @@ def main(args):
         def body_fn(i, val):
             loss_sum, rng = val
             batch = test_fetch(i, data_idx)
-            loss = svi_eval(rng, opt_state, batch, batch) / args.num_samples
-            loss_sum += loss / num_batch
+            loss = svi_eval(rng, opt_state, batch, batch)
+            loss_sum += loss / (args.num_samples * num_batch)
             return loss_sum, rng
 
         loss, _ = lax.fori_loop(0, num_batch, body_fn, (0., rng))
@@ -257,16 +261,16 @@ def main(args):
         t_start = time.time()
         rng, train_rng, data_fetch_rng = random.split(rng, 3)
 
-        num_train, train_idx = train_init(rng=data_fetch_rng)
+        num_train_batches, train_idx = train_init(rng=data_fetch_rng)
         train_loss, opt_state, _ = epoch_train(
-            train_rng, opt_state, train_idx, num_train
+            train_rng, opt_state, train_idx, num_train_batches
         )
 
         if i % 100 == 0:
             rng, test_rng, test_fetch_rng = random.split(rng, 3)
-            num_test, test_idx = test_init(rng=test_fetch_rng)
+            num_test_batches, test_idx = test_init(rng=test_fetch_rng)
             test_loss = eval_test(
-                test_rng, opt_state, test_idx, num_test
+                test_rng, opt_state, test_idx, num_test_batches
             )
             smoothed_loss_window[window_idx] = test_loss
             smoothed_loss = onp.nanmean(smoothed_loss_window)

examples/logistic_regression.py
@@ -38,19 +38,10 @@ def model(batch_X, batch_y=None, num_obs_total=None):
     :param batch_X: a batch of predictors
     :param batch_y: a batch of observations
     """
-    # note(lumip): the following if construct is currently necessary because
-    #   the per-example value_and_grad function uses vmap internally, applying
-    #   model and guide to 1-example batches (and stripping the first dimension)
-    #   this is not nice because it means that model/guide have to be adapted
-    #   if they do the kind of checks as below..
-    if np.ndim(batch_X) == 2:
-        batch_size = example_count(batch_X)
-        assert(batch_y is None or example_count(batch_X) == example_count(batch_y))
-    elif np.ndim(batch_X) == 1:
-        batch_size = 1
-        assert(batch_y is None or example_count(batch_y) == 1)
-
-    z_dim = np.atleast_2d(batch_X).shape[1]
+    assert(np.ndim(batch_X) <= 2)
+    # np.atleast_2d necessary because batch_size dimension is strapped during gradient computation
+    batch_size, z_dim = np.shape(np.atleast_2d(batch_X))
+    assert(batch_y is None or example_count(batch_y) == batch_size)
 
     z_w = sample('w', dist.Normal(np.zeros((z_dim,)), np.ones((z_dim,)))) # prior is N(0,I)
     z_intercept = sample('intercept', dist.Normal(0,1)) # prior is N(0,1)

examples/simple_gaussian_posterior.py
@@ -32,27 +32,19 @@ from datasets import batchify_data
 def model(obs=None, num_obs_total=None, d=None):
     """Defines the generative probabilistic model: p(x|z)p(z)
     """
-    # note(lumip): the following if construct is currently necessary because
-    #   the per-example value_and_grad function uses vmap internally, applying
-    #   model and guide to 1-example batches (and stripping the first dimension)
-    #   this is not nice because it means that model/guide have to be adapted
-    #   if they do the kind of checks as below..
     if obs is not None:
-        if np.ndim(obs) == 2:
-            B = example_count(obs)
-            d = obs.shape[1]
-        elif np.ndim(obs) == 1:
-            B = 1
-            d = obs.shape[0]
+        assert(np.ndim(obs) <= 2)
+        # np.atleast_2d necessary because batch_size dimension is strapped during gradient computation
+        batch_size, d = np.shape(np.atleast_2d(obs))
     else:
         assert(num_obs_total is not None)
-        B = num_obs_total
+        batch_size = num_obs_total
         assert(d is not None)
 
     z_mu = sample('mu', dist.Normal(np.zeros((d,)), 1.))
     x_var = .1
-    with minibatch(B, num_obs_total):
-        x = sample('obs', dist.Normal(z_mu, x_var), obs=obs, sample_shape=(B,))
+    with minibatch(batch_size, num_obs_total):
+        x = sample('obs', dist.Normal(z_mu, x_var), obs=obs, sample_shape=(batch_size,))
     return x
 
 def guide(obs, num_obs_total=None, d=None):
@@ -136,7 +128,7 @@ def main(args):
             batch_loss, opt_state, rng = svi_update(
                 i, update_rng, opt_state, batch, batch,
             )
-            loss += batch_loss / (args.num_samples * args.batch_size * num_batch)
+            loss += batch_loss / (args.num_samples * num_batch)
             return loss, opt_state, rng
 
         return lax.fori_loop(0, num_batch, body_fn, (0., opt_state, rng))
@@ -147,13 +139,12 @@ def main(args):
             loss_sum, rng = val
             batch = test_fetch(i, data_idx)
             rng, eval_rng = jax.random.split(rng, 2)
-            loss = svi_eval(eval_rng, opt_state, batch, batch) / args.num_samples
-            loss_sum += loss
+            loss = svi_eval(eval_rng, opt_state, batch, batch)
+            loss_sum += loss / (args.num_samples * num_batch)
 
             return loss_sum, rng
 
         loss, _ = lax.fori_loop(0, num_batch, body_fn, (0., rng))
-        loss /= num_batch
         return loss
 
 	## Train model
@@ -161,16 +152,16 @@ def main(args):
         t_start = time.time()
         rng, train_rng, data_fetch_rng = random.split(rng, 3)
 
-        num_train, train_idx = train_init(rng=data_fetch_rng)
+        num_train_batches, train_idx = train_init(rng=data_fetch_rng)
         train_loss, opt_state, _ = epoch_train(
-            train_rng, opt_state, train_idx, num_train
+            train_rng, opt_state, train_idx, num_train_batches
         )
 
         if (i % (args.num_epochs // 10) == 0):
             rng, test_rng, test_fetch_rng = random.split(rng, 3)
-            num_test, test_idx = test_init(rng=test_fetch_rng)
+            num_test_batches, test_idx = test_init(rng=test_fetch_rng)
             test_loss = eval_test(
-                test_rng, opt_state, test_idx, num_test
+                test_rng, opt_state, test_idx, num_test_batches
             )
 
             print("Epoch {}: loss = {} (on training set: {}) ({:.2f} s.)".format(

examples/vae.py
@@ -25,6 +25,7 @@ from numpyro.primitives import param, sample
 from numpyro.svi import elbo
 
 from dppp.svi import dpsvi, minibatch
+from dppp.util import example_count
 
 from datasets import MNIST, load_dataset
 
@@ -91,12 +92,12 @@ def model(batch, decode, z_dim, num_obs_total=None, **kwargs):
     #   model and guide to 1-example batches (and stripping the first dimension)
     #   this is not nice because it means that model/guide have to be adapted
     #   if they do the kind of checks as below..
-    if batch.ndim == 3:
-        batch_size = batch.shape[0]
-        batch = np.reshape(batch, (batch_size, -1)) # squash each data item into a one-dimensional array (preserving only the batch size on the first axis)
-    elif batch.ndim == 2:
+    assert(np.ndim(batch) == 3 or np.ndim(batch) == 2)
+    if np.ndim(batch) == 3:
+        batch_size = example_count(batch)
+    else:
         batch_size = 1
-        batch = np.reshape(batch, (-1,))
+    batch = np.reshape(batch, (batch_size, -1)) # squash each data item into a one-dimensional array (preserving only the batch size on the first axis)
 
     decoder_params = param('decoder', None) # advertise/register decoder parameters
     with minibatch(batch_size, num_obs_total=num_obs_total):
@@ -224,7 +225,7 @@ def main(args):
             batch_loss, opt_state, rng = svi_update(
                 i, update_rng, opt_state, (batch,), (batch,),
             )
-            loss += batch_loss / (num_samples * args.batch_size * num_batch)
+            loss += batch_loss / (num_samples * num_batch)
             return loss, opt_state, rng
 
         return lax.fori_loop(0, num_batch, body_fn, (0., opt_state, rng))
@@ -243,11 +244,10 @@ def main(args):
             rng, binarize_rng, eval_rng = random.split(rng, 3)
             batch = test_fetch(i, test_idx, binarize_rng)[0]
             loss = svi_eval(eval_rng, opt_state, (batch,), (batch,))
-            loss_sum += loss / num_samples
+            loss_sum += loss / (num_samples * num_batch)
             return loss_sum, rng
 
         loss, _ = lax.fori_loop(0, num_batch, body_fn, (0., rng))
-        loss /= num_batch
         return loss
 
     def reconstruct_img(epoch, num_epochs, opt_state, rng):
@@ -291,12 +291,14 @@ def main(args):
         rng_shuffle_train, rng_train_init, rng_test_init = random.split(
             rng_shuffle_train, 3
         )
-        num_train, train_idx = train_init(rng=rng_train_init)
-        train_loss, opt_state, rng = epoch_train(rng, opt_state, train_idx, num_train)
+        num_train_batches, train_idx = train_init(rng=rng_train_init)
+        train_loss, opt_state, rng = epoch_train(
+            rng, opt_state, train_idx, num_train_batches
+        )
 
         rng, rng_test, rng_recons = random.split(rng, 3)
-        num_test, test_idx = test_init(rng=rng_test_init)
-        test_loss = eval_test(rng_test, opt_state, test_idx, num_test)
+        num_test_batches, test_idx = test_init(rng=rng_test_init)
+        test_loss = eval_test(rng_test, opt_state, test_idx, num_test_batches)
 
         reconstruct_img(i, args.num_epochs, opt_state, rng_recons)
         print("Epoch {}: loss = {} (on training set: {}) ({:.2f} s.)".format(