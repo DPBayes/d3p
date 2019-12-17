9f333a8a91c376ee6bcf0d9adb490dab5431b87d
Prediger Lukas <lukas.m.prediger@aalto.fi>
2019-12-17 19:41:53 +0200

    Updated setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ from setuptools import find_packages, setup
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-numpyro_commit = '101753fe69f00e8200c27789c570ff607c93cbd3'
-
 setup(
     name='dppp',
     version='0.1.0',
@@ -16,14 +14,11 @@ setup(
     packages=find_packages(include=['dppp', 'dppp.*']),
     author='PADS @ Helsinki University and Aalto University',
     install_requires=[
-        'numpyro @ git+https://github.com/pyro-ppl/numpyro.git@{}#egg=numpyro'.format(numpyro_commit)
+        'numpyro >= 0.2.0'
     ],
     extras_require={
         'examples': ['matplotlib'],
     },
-    dependency_links=[
-        'https://github.com/pyro-ppl/numpyro/tarball/{}#egg=numpyro'.format(numpyro_commit)
-    ],
     long_description="",
     long_description_content_type='text/markdown',
     tests_require=[],
