0ef4d1bbc59d7567c5bf085da65d2ca41e2fd3f3
Lukas Prediger <lukas.m.prediger@aalto.fi>
2021-02-22 14:02:57 +0200

    Fix: per-example gradients no longer use same rng key for each example
    Previously the same rng key was used for each example in a batch.
    This caused problems when sampling local latent variables,
    resulting in the same sample value for each examle.
    
    Removed per_exaple_value_and_grad function in the process
    (is now integrated in DPSVI internally due to being closely coupled).

--- dppp/svi.py
+++ dppp/svi.py
@@ -35,15 +35,6 @@ from collections import namedtuple
 
 DPSVIState = namedtuple('DPSVIState', ('optim_state', 'rng_key', 'observation_scale'))
 
-def per_example_value_and_grad(fun, argnums=0, has_aux=False, holomorphic=False):
-    # vmap removes leading dimensions, we re-add those in a wrapper for fun so
-    # that fun can be oblivious of this
-    def fun_for_vmap(params, args):
-        new_args = (jnp.expand_dims(arg, 0) for arg in args)
-        return fun(params, new_args)
-    value_and_grad_fun = jax.value_and_grad(fun_for_vmap, argnums, has_aux, holomorphic)
-    return jax.vmap(value_and_grad_fun, in_axes=(None, 0))
-
 
 def get_observations_scale(model, model_args, model_kwargs, params):
     """
@@ -253,15 +244,22 @@ class DPSVI(SVI):
         dp_svi_state, rng_key_step = self._split_rng_key(dp_svi_state)
         params = self.optim.get_params(dp_svi_state.optim_state)
 
-        def wrapped_px_loss(x, loss_args):
+        # we wrap the per-example loss (ELBO) to make it easier "digestable"
+        # for jax.vmap(jax.value_and_grad()): slighly reordering parameters; fixing kwargs, model and guide
+        def wrapped_px_loss(prms, rng_key, loss_args):
+            # vmap removes leading dimensions, we re-add those in a wrapper for fun so
+            # that fun can be oblivious of this
+            new_args = (jnp.expand_dims(arg, 0) for arg in loss_args)
             return self.loss.px_loss.loss(
-                rng_key_step, self.constrain_fn(x), self.model, self.guide,
-                *loss_args, **kwargs, **self.static_kwargs
+                rng_key, self.constrain_fn(prms), self.model, self.guide,
+                *new_args, **kwargs, **self.static_kwargs
             )
 
-        per_example_loss, per_example_grads = per_example_value_and_grad(
-            wrapped_px_loss
-        )(params, args)
+        batch_size = jnp.shape(args[0])[0] # todo: need checks to ensure this indexing is okay
+        px_rng_keys = jax.random.split(rng_key_step, batch_size)
+
+        px_value_and_grad = jax.vmap(jax.value_and_grad(wrapped_px_loss), in_axes=(None, 0, 0))
+        per_example_loss, per_example_grads = px_value_and_grad(params, px_rng_keys, args)
 
         return dp_svi_state, per_example_loss, per_example_grads
 
