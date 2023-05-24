f4cf257b4e4caa051d40311fa4ecfbc55b166045
Lukas Prediger <lukas.m.prediger@aalto.fi>
2023-05-24 10:39:44 +0300

    DPSVI.update now accepts a sample mask.
    Updated logistic_regression example to use poisson sampling.

--- tests/test_dpsvi.py
+++ tests/test_dpsvi.py
@@ -28,6 +28,7 @@ import d3p.random.debug
 import numpy as np
 
 from d3p.svi import DPSVI, DPSVIState, full_norm
+from util import are_trees_close
 
 
 class DPSVITestBase:
@@ -39,12 +40,18 @@ class DPSVITestBase:
         self.rng_suite = self.get_rng_suite()
         self.rng = self.rng_suite.PRNGKey(9782346)
         self.batch_size = 10
+        self.num_elements = 8
+        self.mask = jnp.arange(self.batch_size) < self.num_elements
+        self.rescale_factor = self.batch_size / self.num_elements
         self.num_obs_total = 100
         self.px_grads = (
-            jnp.zeros((self.batch_size, 10000)),
-            jnp.zeros((self.batch_size, 10000))
+            jnp.ones((self.batch_size, 10000)),
+            jnp.ones((self.batch_size, 10000))
         )
-        self.px_loss = jnp.arange(self.batch_size, dtype=jnp.float32)
+        self.masked_px_grads = tuple(
+            g * self.mask.reshape(-1, 1) for g in self.px_grads
+        )
+        self.px_loss = jnp.arange(self.batch_size, dtype=jnp.float32) * self.mask
         self.dp_scale = 1.
         self.clipping_threshold = 2.
         self.optim = SGD(1.)
@@ -102,6 +109,40 @@ class DPSVITestBase:
 
         self.assertEqual(jax.tree_util.tree_structure(svi_state.optim_state), jax.tree_util.tree_structure(dpsvi_state.optim_state))
 
+    def test_compute_px_gradients_masking(self):
+        def model(X):
+            mu = numpyro.sample("mu", numpyro.distributions.Normal(np.zeros((X.shape[1],))))
+            with numpyro.plate("plate", size=self.num_obs_total, subsample_size=X.shape[0]):
+                numpyro.sample("X", numpyro.distributions.Normal(mu, 1.).to_event(1), obs=X)
+
+        guide = numpyro.infer.autoguide.AutoDiagonalNormal(model)
+
+        batch = (jnp.ones((self.batch_size, 3)),)
+
+        svi = DPSVI(
+            model, guide, self.optim, numpyro.infer.Trace_ELBO(),
+            self.clipping_threshold, self.dp_scale, rng_suite=self.rng_suite
+        )
+        svi_state = svi.init(self.rng, *batch)
+
+        new_svi_state, px_losses, px_grads, num_elements, batch_mask_scaling_factor = \
+            svi._compute_per_example_gradients(svi_state, svi_state.rng_key, *batch, mask=self.mask)
+        
+        self.assertTrue(are_trees_close(svi_state.optim_state, new_svi_state.optim_state))
+        self.assertEqual(svi_state.observation_scale, new_svi_state.observation_scale)
+        
+        self.assertEqual(self.num_elements, num_elements)
+        self.assertAlmostEqual(self.batch_size / self.num_elements, batch_mask_scaling_factor)
+
+        self.assertFalse(np.allclose(px_losses[:self.num_elements], 0.))
+        self.assertTrue(np.allclose(px_losses[self.num_elements:], 0.))
+
+        self.assertFalse(np.allclose(px_grads["auto_loc"][:self.num_elements], 0.))
+        self.assertTrue(np.allclose(px_grads["auto_loc"][self.num_elements:], 0.))
+
+        self.assertFalse(np.allclose(px_grads["auto_scale"][:self.num_elements], 0.))
+        self.assertTrue(np.allclose(px_grads["auto_scale"][self.num_elements:], 0.))
+
     def test_px_gradient_clipping(self):
         svi_state = DPSVIState(None, self.rng, 0.8)
 
@@ -153,23 +194,26 @@ class DPSVITestBase:
         svi_state = DPSVIState(None, self.rng, .3)
 
         grads = jax.tree_util.tree_map(lambda x: jnp.mean(x, axis=0), self.px_grads)
+        masked_grads = jax.tree_util.tree_map(lambda x: jnp.mean(x, axis=0), self.masked_px_grads)
 
         new_svi_state, perturbed_grads = \
             self.svi._perturb_and_reassemble_gradients(
-                svi_state, self.rng, grads, self.batch_size
+                svi_state, self.rng, masked_grads, self.num_elements, self.rescale_factor
             )
 
         self.assertIs(svi_state.optim_state, new_svi_state.optim_state)
         self.assertEqual(jax.tree_util.tree_structure(grads), jax.tree_util.tree_structure(perturbed_grads))
 
-        expected_std = self.dp_scale * (self.clipping_threshold / self.batch_size) * svi_state.observation_scale
+        corrected_observation_scale = svi_state.observation_scale * self.rescale_factor
+        expected_std = self.dp_scale * (self.clipping_threshold / self.num_elements) * corrected_observation_scale
         for perturbed_site, site in zip(jax.tree_util.tree_leaves(perturbed_grads), jax.tree_util.tree_leaves(grads)):
             self.assertEqual(perturbed_site.shape, site.shape)
             self.assertTrue(
                 np.allclose(expected_std, jnp.std(perturbed_site), atol=1e-2),
                 f"expected stdev {expected_std} but was {jnp.std(perturbed_site)}"
             )
-            self.assertTrue(np.allclose(jnp.mean(site), jnp.mean(perturbed_site), atol=1e-2))
+            # self.assertTrue(np.allclose(jnp.mean(site), jnp.mean(perturbed_site), atol=1e-2))
+            self.assertAlmostEqual(jnp.mean(site) * svi_state.observation_scale, jnp.mean(perturbed_site), places=2)
 
     def test_dp_noise_perturbation_not_deterministic_over_rngs(self):
         """ verifies that different randomness is used in subsequent calls """
@@ -180,12 +224,12 @@ class DPSVITestBase:
 
         new_svi_state, first_grads = \
             self.svi._perturb_and_reassemble_gradients(
-                svi_state, first_rng, grads, self.batch_size
+                svi_state, first_rng, grads, self.num_elements, self.rescale_factor
             )
 
         _, second_grads = \
             self.svi._perturb_and_reassemble_gradients(
-                new_svi_state, second_rng, grads, self.batch_size
+                new_svi_state, second_rng, grads, self.num_elements, self.rescale_factor
             )
 
         some_gradient_noise_is_equal = reduce(
@@ -206,7 +250,7 @@ class DPSVITestBase:
 
         _, grads = \
             self.svi._perturb_and_reassemble_gradients(
-                svi_state, self.rng, grads, self.batch_size
+                svi_state, self.rng, grads, self.num_elements, self.rescale_factor
             )
 
         noise_sites = jax.tree_util.tree_leaves(grads)
