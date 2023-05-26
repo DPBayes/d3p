0eec8d668006cda23bb440ad62185e6e0ec3ade3
Lukas Prediger <lukas.m.prediger@aalto.fi>
2023-05-26 18:43:20 +0300

    Added secure randint sampling routine to d3p.random

--- d3p/random/__init__.py
+++ d3p/random/__init__.py
@@ -81,6 +81,71 @@ def _normal(key, shape, dtype) -> jnp.ndarray:
     return np.array(np.sqrt(2), dtype) * jax.lax.erf_inv(u)
 
 
+def randint(key: ccrng.RNGState,
+            shape: Sequence[int],
+            minval: Union[jnp.int16, jnp.int32, jnp.int64],
+            maxval: Union[jnp.int16, jnp.int32, jnp.int64],
+            dtype: np.dtype = jnp.int32) -> jnp.ndarray:
+    """Uniformly samples integer values in given bounds with given shape
+        derived from a cryptographically-secure pseudo-random number generator.
+
+    :param key: A PRNGKey used as the random key.
+    :param shape: A tuple of nonnegative integers representing the result
+        shape.
+    :param minval: Minimum possible value of samples..
+    :param maxval: Maximum possible value of samples.
+    :param dtype: Optional. An integer dtype for the returned values (default `int32`).
+
+    :return: A random array with the specified shape and dtype.
+    """
+    if not jax.dtypes.issubdtype(dtype, np.integer):
+        raise TypeError(f"dtype argument to `randint` must be an integer dtype, got {dtype}")
+    dtype = jax.dtypes.canonicalize_dtype(dtype)
+    return _randint(key, minval, maxval, shape, dtype)
+
+
+
+@partial(jax.jit, static_argnums=(3, 4))
+def _randint(key, minval,  maxval, shape, dtype) -> jnp.ndarray:
+    if not jax.dtypes.issubdtype(dtype, np.integer):
+        raise TypeError(f"dtype argument to `randint` must be an integer dtype, got {dtype}")
+    dtype = jax.dtypes.canonicalize_dtype(dtype)
+    iinfo = jnp.iinfo(dtype)
+    nbits = iinfo.bits
+    assert nbits in (8, 16, 32, 64)
+
+    vdtype, udtype = {
+         8: (jnp.int8,  jnp.uint8 ),
+        16: (jnp.int16, jnp.uint16),
+        32: (jnp.int32, jnp.uint32),
+        64: (jnp.int64, jnp.uint64)
+    }[nbits]
+
+    delta = udtype(maxval - 1 - minval)
+    log_delta_next_po2 = jnp.minimum(udtype(jnp.log2(jnp.float32(delta)) + 1), nbits)
+
+    delta_next_po2 = udtype(1) << log_delta_next_po2
+    bitmask = delta_next_po2 - udtype(1)
+
+    def sampling_condition(args):
+        _, uvals = args
+        return jnp.any((uvals > delta)) # & (uvals > 0))
+
+    def sampling_body(args):
+        key, uvals = args
+        key, round_key = ccrng.split(key, 2)
+        new_uvals = jax.lax.bitwise_and(udtype(ccrng.random_bits(round_key, nbits, shape)), bitmask)
+        uvals = jnp.where(uvals > delta, new_uvals, uvals)
+        return key, uvals
+    
+    key, round_key = ccrng.split(key, 2)
+    uvals = jax.lax.bitwise_and(udtype(ccrng.random_bits(round_key, nbits, shape)), bitmask)
+    _, uvals = jax.lax.while_loop(sampling_condition, sampling_body, (key, uvals))
+
+    vals = vdtype(uvals) + minval
+    return vals
+
+
 def convert_to_jax_rng_key(rng_key: ccrng.RNGState) -> jnp.array:
     """Converts a give d3p.random RNG key to a jax.random RNG key.
 
